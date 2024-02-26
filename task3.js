2e3fa1f2e1a6b0214313796ce33685ef632059c3
Elena Musat <emailelena_catalina_musat@yahoo.com>
2024-02-26 21:47:00 +0200

    first commit

---
+++ task3.js
@@ -0,0 +1,25 @@
+/*
+    Create a simple card with an image and text content, and use only the <style> 
+    tag within the HTML file to apply styles.
+
+
+    HTML Structure:
+        Create a <div> element to represent the card.
+
+        Inside the card, include an <img> element for a picture and a <div> for 
+        text content.
+
+        Add a heading (<h3>) and a paragraph (<p>) within the text <div>.
+
+
+    Style Tag:
+        Use only the <style> tag within the HTML file
+        Set a width of 600px to the card and center it;
+        Set a border of 2px solid #333 to the card.
+        Add padding of 15px to the card.
+        Set a background color for the card to #f0f0f0.
+        Style the heading text with a color of blue and font size of 18px.
+        Style the paragraph text with a color of #555 and font size of 14px.
+        Set the border-radius of the card to 8px.
+        Style the image to have a width of 450px and a border-radius of 8px.
+*/
